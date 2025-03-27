84ccff47b9f0c664419fe83f0024514e0a1df81d
Nick <n.battista180@gmail.com>
2025-03-26 18:01:48 -0600

    scaffold frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { fetchUsers } from "./api";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetchUsers()
+      .then(setUsers)
+      .catch((err) => console.error("Error fetching users:", err));
+  }, []);
+
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold">Users</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id} className="border p-2 my-2">
+            {user.firstName} {user.lastName}- {user.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
